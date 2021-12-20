a99f2b77f5256afe833c72c619c42ad66a7a6eb2
gelo-gels <riangeloo17@gmail.com>
2021-12-20 20:11:37 +0800

    test 2

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -503,24 +503,6 @@ export default {
   methods: {
   	
   },
-  mounted() {
-	  function encode(data) {
-		return Object.keys(data)
-			.map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-			.join("&")
-	}
-	const handleSubmit = (event) => {
-	event.preventDefault()
-	fetch("/", {
-		method: "POST",
-		headers: { "Content-Type": "application/x-www-form-urlencoded" },
-		body: encode({
-		"form-name": event.target.getAttribute("name"),
-		...name
-		})
-	}).then(() => navigate("/thank-you/")).catch(error => alert(error))
-	}
-  }
   
 }
 </script>
